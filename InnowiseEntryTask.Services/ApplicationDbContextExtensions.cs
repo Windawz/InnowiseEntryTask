685b496317108e6a58f763d8e658fa825c72effc
Windawz <46968683+Windawz@users.noreply.github.com>
2023-10-16 13:50:52 +0300

    Write type-based DbSet lookup extension method

---
+++ InnowiseEntryTask.Services/ApplicationDbContextExtensions.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using InnowiseEntryTask.Data;
+using System.Reflection;
+
+namespace InnowiseEntryTask.Services;
+
+internal static class ApplicationDbContextExtensions
+{
+    private static readonly IReadOnlyDictionary<Type, Func<ApplicationDbContext, object>> _dbSetGettersPerType =
+        new Dictionary<Type, Func<ApplicationDbContext, object>>
+        {
+            { typeof(Album), context => context.Albums },
+            { typeof(Artist), context => context.Artists },
+            { typeof(Song), context => context.Songs },
+        };
+
+    public static DbSet<TEntity> DbSetOf<TEntity>(this ApplicationDbContext context) where TEntity : class
+    {
+        var entityType = typeof(TEntity);
+        if (_dbSetGettersPerType.TryGetValue(entityType, out var getter))
+        {
+            return (DbSet<TEntity>)getter(context);
+        }
+        else
+        {
+            return FindDbSetAmongProperties<TEntity>(context)
+                ?? throw new ArgumentException(
+                    message: $"ApplicationDbContext does not have a DbSet of type {typeof(TEntity)}",
+                    paramName: nameof(context)
+                );
+        }
+    }
+
+    private static DbSet<TEntity>? FindDbSetAmongProperties<TEntity>(ApplicationDbContext context) where TEntity : class
+    {
+        PropertyInfo? propertyInfo = context.GetType()
+            .GetProperties()
+            .FirstOrDefault(propertyInfo => propertyInfo.PropertyType == typeof(DbSet<TEntity>));
+        
+        if (propertyInfo is null)
+        {
+            return null;
+        }
+
+        object? rawValue = propertyInfo.GetValue(context);
+
+        if (rawValue is null or not DbSet<TEntity>)
+        {
+            throw new InvalidOperationException("Got invalid value when getting DbSet from ApplicationDbContext through reflection");
+        }
+
+        return (DbSet<TEntity>)rawValue;
+    }
+}
