8390b8cd0aadce4817ba01c85ff416b1e5307748
Windawz <46968683+Windawz@users.noreply.github.com>
2023-10-22 19:59:12 +0300

    Remove generic crud controller class

--- InnowiseEntryTask.WebApi/Controllers/AlbumController.cs
+++ InnowiseEntryTask.WebApi/Controllers/AlbumController.cs
@@ -4,8 +4,60 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InnowiseEntryTask.WebApi.Controllers;
 
+[ApiController]
 [Route("/Albums")]
-public class AlbumController : CrudController<Album>
+public class AlbumController : ControllerBase
 {
-    public AlbumController(Crud<Album> crud) : base(crud) { }
+    public AlbumController(Crud<Album> crud)
+    {
+        _crud = crud;
+    }
+
+    private readonly Crud<Album> _crud;
+
+    [HttpPost]
+    public IActionResult Post(Album album)
+    {
+        _crud.Create(album);
+        return CreatedAtAction(nameof(Get), album);
+    }
+
+    [HttpGet]
+    public IReadOnlyCollection<Album> GetAll() =>
+        _crud.ReadAll();
+
+    [HttpGet("{id}")]
+    public Album? Get(int id) =>
+        _crud.Read(id);
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, Album album)
+    {
+        if (album.Id != id)
+        {
+            album.Id = id;
+        }
+
+        if (_crud.Update(album))
+        {
+            return Ok();
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        if (_crud.Delete(id))
+        {
+            return Ok();
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
 }
--- InnowiseEntryTask.WebApi/Controllers/SongController.cs
+++ InnowiseEntryTask.WebApi/Controllers/SongController.cs
@@ -4,8 +4,60 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InnowiseEntryTask.WebApi.Controllers;
 
+[ApiController]
 [Route("/Songs")]
-public class SongController : CrudController<Song>
+public class SongController : ControllerBase
 {
-    public SongController(Crud<Song> crud) : base(crud) { }
+    public SongController(Crud<Song> crud)
+    {
+        _crud = crud;
+    }
+
+    private readonly Crud<Song> _crud;
+
+    [HttpPost]
+    public IActionResult Post(Song song)
+    {
+        _crud.Create(song);
+        return CreatedAtAction(nameof(Get), song);
+    }
+
+    [HttpGet]
+    public IReadOnlyCollection<Song> GetAll() =>
+        _crud.ReadAll();
+
+    [HttpGet("{id}")]
+    public Song? Get(int id) =>
+        _crud.Read(id);
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, Song song)
+    {
+        if (song.Id != id)
+        {
+            song.Id = id;
+        }
+
+        if (_crud.Update(song))
+        {
+            return Ok();
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        if (_crud.Delete(id))
+        {
+            return Ok();
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
 }
